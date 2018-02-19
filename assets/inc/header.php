1c3df6f97a71a18fb42c8493f2e872b30b4a5cfb
Roxane Prestat <roxane.ruralwebfactory@gmail.com>
2018-02-19 16:45:41 +0100

    ajustement des liens nav

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -24,7 +24,7 @@
        <!-- En-tête logo
    ================================================== -->
    <div class="row" id="header">
-         <a href="index(1).html"><img src="assets/img/logo-rwf.png" alt="" id="rwf"/></a>
+         <a href="./index.php"><img src="assets/img/logo-rwf.png" alt="" id="rwf"/></a>
          <a id="reseauxsociaux1" class="btn btn-default" href="https://fr-fr.facebook.com/RuralWebFactory/"><i class="fa fa-facebook fa-2x"></i></a>
          <a id="reseauxsociaux2" class="btn btn-default" href="https://twitter.com/ruralwebfactory"><i class="fa fa-twitter fa-2x"></i></a>
    </div>
@@ -38,33 +38,32 @@
          <span class="icon-bar"></span>
          <span class="icon-bar"></span>
        </button>
-       <a class="navbar-brand" href="index(1).html">Accueil</a>
+       <a class="navbar-brand" href="./index.php">Accueil</a>
      </div>
 
      <div class="collapse navbar-collapse">
        <ul class="nav navbar-nav">
-         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="qui.html">Qui sommes-nous ?<span class="caret"></span></a>
+         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="./qui.php">Qui sommes-nous ?<span class="caret"></span></a>
            <ul class="dropdown-menu">
-               <li><a href="concept.html">Concept</a></li>
-               <li><a href="faq.html">FAQ</a></li>
+               <li><a href="./concept.php">Concept</a></li>
+               <li><a href="./faq.php">FAQ</a></li>
            </ul>
          </li>
-         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="ecole.html">L'école<span class="caret"></span></a>
+         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="./ecole.php">L'école<span class="caret"></span></a>
            <ul class="dropdown-menu">
-               <li><a href="projets.html">Projets</a></li>
-               <li><a href="equipe.html">L'équipe</a></li>
-               <li><a href="formation.html">La formation</a></li>
+               <li><a href="./projets.php">Projets</a></li>
+               <li><a href="./equipe.php">L'équipe</a></li>
+               <li><a href="./formation.php">La formation</a></li>
            </ul>
          </li>
-         <li> <a href="#">E-learning</a> </li>
          <li> <a href="#">Espace membre</a> </li>
-         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="candidature.html">Candidatures<span class="caret"></span></a>
+         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="./candidature.php">Candidatures<span class="caret"></span></a>
            <ul class="dropdown-menu">
-               <li><a href="candidature1.html" id="interesse">La formation vous intéresse ?</a></li>
-               <li><a href="candidature2.html" id="entreprise">Vous êtes une entreprise ?</a></li>
+               <li><a href="./candidature1.php" id="interesse">La formation vous intéresse ?</a></li>
+               <li><a href="./candidature2.php" id="entreprise">Vous êtes une entreprise ?</a></li>
            </ul>
          </li>
-         <li> <a href="contacts.html">Contacts</a> </li>
+         <li> <a href="./contacts.php">Contacts</a> </li>
        </ul>
      </div>
    </nav>
