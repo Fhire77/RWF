7654b1291fc2a15067943c43f10206739993f802
Roxane Prestat <roxane.ruralwebfactory@gmail.com>
2018-02-20 16:27:42 +0100

    modif contact.php

--- contacts.php
+++ contacts.php
@@ -4,8 +4,8 @@
    ================================================== -->
    <section class="row" id="candidature">
        <div class="col-lg-12" id="para1">
-         <h1>Adresse postale de l'école</h1>
-         <p></p>
+         <h1>Adresse de l'école</h1>
+         <p>Rural web factory <br>rue moignard <br> 16390 Aubeterre-sur-dronne</p>
        </div>
 
 
@@ -14,16 +14,31 @@
 
        <div class="col-lg-12" id="para2">
          <h1>Mini map de localité</h1>
-         <p></p>
        </div>
-
 
      <!--Para #3
    ================================================== -->
 
        <div class="col-lg-12" id="para3">
          <h1>Formulaire de contact</h1>
-         <p></p>
+         <br /><br />
+        <form name="contact" method="post" action="mailto:" enctype="text/plain">
+
+         <input type="text" name="nom" placeholder="Nom" style="width:450px;height:3em;"/>  <br/><br/>
+         <input type="text" name="prenom"  placeholder="Prénom" style="width:450px;height:3em;"/>
+          <br/><br/>
+         <input type="text" name="email"  placeholder="Email" style="width:450px;height:3em;"/>
+          <br/><br/>
+         <input type="text" name="tel"  placeholder="Téléphone" style="width:450px;height:3em;"/>
+          <br/><br/>
+         <textarea id="message" name="user_message" placeholder="Message " style="width:600px;height:12em;"></textarea>
+          <br/><br/>
+
+         <div class="button">
+          <button type="submit" style="margin-left:300px;">Envoyer </button>
+         </div>
+
+        </form>
        </div>
 
 
