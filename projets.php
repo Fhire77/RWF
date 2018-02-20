0fe72c8a76bcc958d6e66c6f5c1973a19822ddce
Roxane Prestat <roxane.ruralwebfactory@gmail.com>
2018-02-20 13:50:07 +0100

    ajout new bandeau fixe

--- projets.php
+++ projets.php
@@ -1,11 +1,15 @@
 <?php include('assets/inc/header.php'); ?>
      <!-- Section #1
    ================================================== -->
-   <section class="row jumbotron" id="">
-       <div class="col-lg-12">
-       <h1>Projets</h1>
+   <section class="row" id="candidature">
+       <div class="col-lg-12" id="para1">
+         <h1>Projets</h1><br><br>
+         <p>- Décembre 2017 :<br>
+            Création du site de la Rural Web Factory qui servira aux futurs promotions</p>
+            <br>
+         <p>- Janvier 2018 :<br>
+            Refonte du site de l'association L'oison de Montmoreau St Cybard</p>
 
-       <p>Recenser les divers projets sur court ou long terme, présent ou à venir ?</p>
        </div>
    </section>
 
