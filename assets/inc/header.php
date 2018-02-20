b5da6ad3b00fcec6f202eece62db3106b12be5c6
Champaloux Benjamin <tiitanyum@gmail.com>
2018-02-20 14:54:10 +0100

    rec header

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -8,14 +8,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
      <title>Rural Web Factory</title>
    <meta name="description" content="Rural Web Factory">
-<<<<<<< HEAD
-   <link rel="stylesheet" type="text/css" href="style.css">
-   <link rel="stylesheet" type="text/css" href="assets/css/pages_secondaires.css">
-   <link href="https://fonts.googleapis.com/css?family=Dosis|Indie+Flower|Josefin+Sans" rel="stylesheet">
-=======
    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
    <!--<link rel="stylesheet" href="MaquetteV2.css" media="screen" /> -->
->>>>>>> bandeau-la-galere
    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
@@ -25,60 +19,6 @@
 
  <body data-spy="scroll" data-target=".navbar">
 
-<<<<<<< HEAD
- <div class="container-fluid">
-
-       <!-- En-tête logo
-   ================================================== -->
-   <div class="row" id="header">
-         <a href="index.php"><img src="assets/img/logo-rwf.png" alt="" id="rwf"/></a>
-         <a id="reseauxsociaux1" class="btn btn-default" href="https://fr-fr.facebook.com/RuralWebFactory/"><i class="fa fa-facebook fa-2x"></i></a>
-         <a id="reseauxsociaux2" class="btn btn-default" href="https://twitter.com/ruralwebfactory"><i class="fa fa-twitter fa-2x"></i></a>
-   </div>
-   <!-- Navigation
-   ================================================== -->
-   <section class="row" id="nav">
-   <nav class="navbar navbar-inverse" role="navigation">
-     <div class="navbar-header">
-       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-       </button>
-       <a class="navbar-brand" href="index.php">Accueil</a>
-     </div>
-
-     <div class="collapse navbar-collapse">
-       <ul class="nav navbar-nav">
-         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="qui.php">Qui sommes-nous ?<span class="caret"></span></a>
-           <ul class="dropdown-menu">
-               <li><a href="qui.php">La Rural Web Factory</a></li>
-               <li><a href="concept.php">Concept</a></li>
-               <li><a href="faq.php">FAQ</a></li>
-           </ul>
-         </li>
-         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="ecole.php">L'école<span class="caret"></span></a>
-           <ul class="dropdown-menu">
-               <li><a href="ecole.php">La structure</a></li>
-               <li><a href="projets.php">Projets</a></li>
-               <li><a href="equipe.php">L'équipe</a></li>
-               <li><a href="formation.php">La formation</a></li>
-           </ul>
-         </li>
-         <li> <a href="#">Espace membre</a> </li>
-         <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="candidature.php">Candidatures<span class="caret"></span></a>
-           <ul class="dropdown-menu">
-               <li><a href="candidature.php" id="candidater">Comment candidater ?</a></li>
-               <li><a href="candidature1.php" id="interesse">La formation vous intéresse ?</a></li>
-               <li><a href="candidature2.php" id="entreprise">Vous êtes une entreprise ?</a></li>
-           </ul>
-         </li>
-         <li> <a href="contacts.php">Contacts</a> </li>
-       </ul>
-     </div>
-   </nav>
-   </section>
-=======
  <div>
 
 
@@ -133,4 +73,3 @@
        </div>
      </nav>
    </div>
->>>>>>> bandeau-la-galere
