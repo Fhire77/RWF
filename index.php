e242d376a651bf2d824f17d8ea6455baafe1cc40
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-03-12 09:35:59 +0100

    Changement partenaires

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php require 'assets/inc/header.php' ?>
 
-   <!-- Section bandeau================================================== -->
+<!-- Section bandeau================================================== -->
 
- <div class="bandeau">
-   <img src="assets/img/bandeauV8.PNG" alt="RWF-Bandeau" id="img-bandeau">
- </div>
+<div class="bandeau">
+ <img src="assets/img/bandeauV8.PNG" alt="RWF-Bandeau" id="img-bandeau">
+</div>
 
-   <!-- Caroussel================================================== -->
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+<!-- Caroussel================================================== -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -40,24 +40,24 @@
 
 
 <!-- Section Présentation
-   ================================================== -->
+ ================================================== -->
 
-   <div class="article">
-     <div class="content">
-       <h1>La «Rural Web Factory», c’est quoi ?</h1>
-       <p>C’est une école innovante de codage, 100% gratuite, basée à Aubeterre-sur-Dronne, à destination des demandeurs d’emploi et/ou des bénéficiaires de minimas sociaux, jeunes ou séniors, hommes ou femmes.
-        <br>
-        <br> C’est une formation courte et intensive de 6 mois qui vise, par des méthodes pédagogiques nouvelles, à apprendre à développer des sites Internet et des applications mobiles pour devenir développeur web, développeur nouvelles technologies ou informaticien de développement.
-        <br>
-        <br> La « Rural Web Factory » bénéficie du label Grande Ecole du Numérique délivré par l’Etat, lui conférant une reconnaissance nationale.</p>
-      </div>
-   </div>
+ <div class="article">
+   <div class="content">
+     <h1>La «Rural Web Factory», c’est quoi ?</h1>
+     <p>C’est une école innovante de codage, 100% gratuite, basée à Aubeterre-sur-Dronne, à destination des demandeurs d’emploi et/ou des bénéficiaires de minimas sociaux, jeunes ou séniors, hommes ou femmes.
+      <br>
+      <br> C’est une formation courte et intensive de 6 mois qui vise, par des méthodes pédagogiques nouvelles, à apprendre à développer des sites Internet et des applications mobiles pour devenir développeur web, développeur nouvelles technologies ou informaticien de développement.
+      <br>
+      <br> La « Rural Web Factory » bénéficie du label Grande Ecole du Numérique délivré par l’Etat, lui conférant une reconnaissance nationale.</p>
+    </div>
+  </div>
 
 
-   <div class="article" id="article2">
-     <div class="content">
-       <h1>Comment s’inscrire ?</h1>
-       <p>La formation est ouverte à une promotion de 24 apprenants. Pour y accéder, vous devez remplir le formulaire d’inscription en cliquant <a href="#" target="_blank">ICI</a>.
+  <div class="article" id="article2">
+   <div class="content">
+     <h1>Comment s’inscrire ?</h1>
+     <p>La formation est ouverte à une promotion de 24 apprenants. Pour y accéder, vous devez remplir le formulaire d’inscription en cliquant <a href="#" target="_blank">ICI</a>.
        <br> Vous devez également vous inscrire obtenir sur le site www.codecademy.fr et obtenir 20 « badges » minimum (10 badges sur le parcours « Html et CSS » et 10 badges sur le parcours et « PHP ».
        <br>
        <br> Vous pouvez télécharger le tuto d’utilisation de la plateforme codecademy en cliquant <a href="http://www.lavalette-tude-dronne.fr/wp-content/uploads/2017/06/Tutoriel-Codecademy-v2.pdf" target="_blank">ICI</a>.
@@ -65,61 +65,79 @@
        <br>
        <br> Les candidatures sont ouvertes du 21 juin au 31 juillet 2017 à minuit.
        <br> Plus d’infos sur <a href="https://simplon.co/comment-candidater/" target="_blank">simplon.co/comment-candidater</a>.</p>
-   </div>
+     </div>
    </div>
 
    <!-- Section logo e-learning
-   ================================================== -->
-      <div class="e-learning">
-         <h1>Catalogue de cours e-learning :</h1>
-        <div class="list-elearn">
-          <a href="https://openclassrooms.com/courses/apprenez-a-creer-votre-site-web-avec-html5-et-css3"  target="_blank"><img id="html" class="img-el" src="assets/img/html5-logo-EF92D240D7-seeklogo.com.png"/></a>
-          <a href="https://openclassrooms.com/courses/apprenez-a-creer-votre-site-web-avec-html5-et-css3"  target="_blank"><img id="css" class="img-el" src="assets/img/CSS3.png"/></a>
-          <a href="https://openclassrooms.com/courses/dynamisez-vos-sites-web-avec-javascript"  target="_blank"><img id="javascript" class="img-el" src="assets/img/js3.png"/></a>
-          <a href="https://openclassrooms.com/courses/concevez-votre-site-web-avec-php-et-mysql"  target="_blank"><img id="php" class="img-el" src="assets/img/logo_php.png"/></a>
-          <a href="https://openclassrooms.com/courses/apprenez-a-programmer-en-python"  target="_blank"><img id="python" class="img-el" src="assets/img/Python-Logo-PNG-Image.png"/></a>
-        </div>
+     ================================================== -->
+     <div class="e-learning">
+       <h1>Catalogue de cours e-learning :</h1>
+       <div class="list-elearn">
+        <a href="https://openclassrooms.com/courses/apprenez-a-creer-votre-site-web-avec-html5-et-css3"  target="_blank"><img id="html" class="img-el" src="assets/img/html5-logo-EF92D240D7-seeklogo.com.png"/></a>
+        <a href="https://openclassrooms.com/courses/apprenez-a-creer-votre-site-web-avec-html5-et-css3"  target="_blank"><img id="css" class="img-el" src="assets/img/CSS3.png"/></a>
+        <a href="https://openclassrooms.com/courses/dynamisez-vos-sites-web-avec-javascript"  target="_blank"><img id="javascript" class="img-el" src="assets/img/js3.png"/></a>
+        <a href="https://openclassrooms.com/courses/concevez-votre-site-web-avec-php-et-mysql"  target="_blank"><img id="php" class="img-el" src="assets/img/logo_php.png"/></a>
+        <a href="https://openclassrooms.com/courses/apprenez-a-programmer-en-python"  target="_blank"><img id="python" class="img-el" src="assets/img/Python-Logo-PNG-Image.png"/></a>
       </div>
-
-
-
-
-   <!-- Section Partenaire================================================== -->
-   <div class="partenaire">
-
-      <h1>Partenaires</h1>
-
-      <div class="logo-part">
-       <a href="https://simplon.co/" target="_blank"><img class="img-part" src="assets/img/logo_simplon.png"/></a>
-       <a href="http://www.cdc4b.com/Economie%20sud%20Charente%20-%20maison%20communautaire%20pour%20l%27emploi" target="_blank"><img class="img-part" src="assets/img/logo-aaisc-300x263.jpeg"/></a>
-       <a href="http://www.capemploi16.com/" target="_blank"><img class="img-part" src="assets/img/capemploi.png"/></a>
-      </div>
-      <div class="logo-part">
-       <a href="https://www.lacharente.fr/" target="_blank"><img class="img-part" src="assets/img/Charente_(16)_logo_2012.png"/></a>
-       <a href="http://www.espace-mobile.org/" target="_blank"><img class="img-part" src="assets/img/logo-ENSC.png"/></a>
-       <a href="http://www.mlarccharente.com/" target="_blank"><img class="img-part" src="assets/img/logo-mission-locale-300x227.png"/></a>
-      </div>
-      <div class="logo-part">
-       <a href="https://www.pole-emploi.fr/accueil/" target="_blank"><img class="img-part" src="assets/img/logo-pole-emploi.gif139x90.png"/></a>
-       <a href="https://www.nouvelle-aquitaine.fr/" target="_blank"><img class="img-part" src="assets/img/logo_NA-horizontal_Coul-300x129.png"/></a>
-       <a href="http://www.lavalette-tude-dronne.fr/" target="_blank"><img class="img-part" src="assets/img/Logo_TD_fd_transparent2.png"/></a>
-      </div>
-
     </div>
 
 
 
 
+    <!-- Section Partenaire================================================== -->
+    <div class="partenaire">
 
-     <!-- Footer ================================================== -->
-    <?php require 'assets/inc/footer.php' ?>
+      <h1>Partenaires</h1>
+
+      <div class="logo-part">
+
+        <a href="http://www.lavalette-tude-dronne.fr/" target="_blank"><img class="img-part" src="assets/img/Logo_TD_fd_transparent2.png"/></a>
+
+        <a href="https://simplon.co/" target="_blank"><img class="img-part" src="assets/img/logo_simplon.png"/></a>
+
+
+
+        <a href="https://www.nouvelle-aquitaine.fr/" target="_blank"><img class="img-part" src="assets/img/logo_NA-horizontal_Coul-300x129.png"/></a>
+      </div>
+
+      <div class="logo-part">
+
+       <a href="https://www.lacharente.fr/" target="_blank"><img class="img-part" src="assets/img/Charente_(16)_logo_2012.png"/></a>
+
+       <a href="http://www.espace-mobile.org/" target="_blank"><img class="img-part" src="assets/img/logo-ENSC.png"/></a>
+
+       
+
+       <a href="https://www.pole-emploi.fr/accueil/" target="_blank"><img class="img-part" src="assets/img/logo-pole-emploi.gif139x90.png"/></a>
+     </div>
+
+     <div class="logo-part">
+
+      <a href="http://www.capemploi16.com/" target="_blank"><img class="img-part" src="assets/img/capemploi.png"/></a>
+
+      <a href="http://www.mlarccharente.com/" target="_blank"><img class="img-part" src="assets/img/logo-mission-locale-300x227.png"/></a>
+
+      <a href="http://www.cdc4b.com/Economie%20sud%20Charente%20-%20maison%20communautaire%20pour%20l%27emploi" target="_blank"><img class="img-part" src="assets/img/logo-aaisc-300x263.jpeg"/></a>
+
+
+
+    </div>
+
+  </div>
 
 
 
 
 
-    <!-- Scripts ================================================== -->
+  <!-- Footer ================================================== -->
+  <?php require 'assets/inc/footer.php' ?>
 
- </body>
+
+
+
+
+  <!-- Scripts ================================================== -->
+
+</body>
 
 </html>
